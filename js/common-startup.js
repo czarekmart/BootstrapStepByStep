13ae6e95cef532ebce0a75c337614fe0eb1c4050
Cezar Mart <czarekmart@gmail.com>
2016-03-03 22:33:28 -0800

    Chapter 11

--- js/common-startup.js
+++ js/common-startup.js
@@ -2,8 +2,27 @@
  * Created by Cezar on 1/19/2016.
  */
 
-// This shows how to include another html
-var $includedLoginForm = $("#includedLoginForm");
-if ( $includedLoginForm ) {
-    $("#includedLoginForm").load("login-form.html");
-}
+
+
+$(function() {
+
+    // This shows how to include another html
+    var $includedLoginForm = $("#includedLoginForm");
+    if ( $includedLoginForm ) {
+        $("#includedLoginForm").load("login-form.html");
+    }
+
+    $(".pop-image").click(function(evt){
+        var $img = $(this).find('img').first();
+        if ( $img ) {
+            var imgPath = $img.attr('src');
+            var imgCaption = $img.attr('alt');
+            if (imgPath) {
+                $('#imagepreview').attr('src', imgPath);
+                $('#imagemodal').modal('show');
+                $('#myModalLabel').text(imgCaption);
+            }
+        }
+    });
+
+});
